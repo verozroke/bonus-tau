b826ef6b14feffeac07ac2e2a6d3151a4bfc7096
verozroke <73778760+verozroke@users.noreply.github.com>
2024-04-13 14:24:04 +0500

    pages added

---
+++ pages/my-cards.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    my-cards
+  </div>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+
+</script>
+
+<style scoped></style>
---
+++ pages/profile.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    profile
+  </div>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+
+</script>
+
+<style scoped></style>
